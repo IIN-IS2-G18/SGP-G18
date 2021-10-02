daab8682e1b771593321e4ddee788b5541a9e1aa
marcelom <marcemolas@gmail.com>
2021-10-02 08:10:56 -0400

    se agregó el jsonableresponsemixin para entender porqué la el form no guardaba a la base de datos. Aún no se guarda.

--- apps/proyectos/forms.py
+++ apps/proyectos/forms.py
@@ -1,8 +1,36 @@
 from django.forms import ModelForm
 from .models import Proyecto
+from django.http import JsonResponse
 
 
-class ProyectoForm(ModelForm):
+class JsonableResponseMixin:
+    """
+    Mixin to add JSON support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        print(form.errors)
+        if self.request.accepts('text/html'):
+            return response
+        else:
+            return JsonResponse(form.errors, status=400)
+
+    def form_valid(self, form):
+        # We make sure to call the parent's form_valid() method because
+        # it might do some processing (in the case of CreateView, it will
+        # call form.save() for example).
+        response = super().form_valid(form)
+        if self.request.accepts('text/html'):
+            return response
+        else:
+            data = {
+                'pk': self.object.pk,
+            }
+            return JsonResponse(data)
+
+
+class ProyectoForm(JsonableResponseMixin,ModelForm):
     class Meta:
         model = Proyecto
         fields = '__all__'
