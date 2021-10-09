02dd675f5bd27a290363f41dea6d83f2c2595b5b
marcelom <marcemolas@gmail.com>
2021-10-09 18:09:11 -0300

    importe modelchoicefield para elegir un equipo y selectdatewidget para poder utilizar el calendario y elegir una fecha. Elimine JsonableResponseMixin pq no aportaba nada al proyecto aun

--- apps/proyectos/forms.py
+++ apps/proyectos/forms.py
@@ -1,35 +1,14 @@
-from django.forms import ModelForm
-from .models import Proyecto
+from django import forms
+from django.forms import ModelForm, ModelChoiceField, SelectDateWidget
+from .models import Proyecto, Equipo
 from django.http import JsonResponse
 
-class JsonableResponseMixin:
-    """
-    Mixin to add JSON support to a form.
-    Must be used with an object-based FormView (e.g. CreateView)
-    """
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        print(form.errors)
-        if self.request.accepts('text/html'):
-            return response
-        else:
-            return JsonResponse(form.errors, status=400)
-
-    def form_valid(self, form):
-        # We make sure to call the parent's form_valid() method because
-        # it might do some processing (in the case of CreateView, it will
-        # call form.save() for example).
-        response = super().form_valid(form)
-        if self.request.accepts('text/html'):
-            return response
-        else:
-            data = {
-                'pk': self.object.pk,
-            }
-            return JsonResponse(data)
 
 
-class ProyectoForm(JsonableResponseMixin, ModelForm):
+class ProyectoForm(ModelForm, forms.Form):
+    equipo = ModelChoiceField(initial = 0, queryset=Equipo.objects.all())
+    fechaInicio = SelectDateWidget()
+    fechaFin = SelectDateWidget()
     class Meta:
         model = Proyecto
         fields = '__all__'
@@ -41,3 +20,4 @@ class ProyectoForm(JsonableResponseMixin, ModelForm):
             'fechaFin': 'Fecha de Finalizacion',
             'estado' : 'Estado'
         }
+
